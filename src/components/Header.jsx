fb3e2f0868d1e5353d8da1df95ec75fee7c1c4aa
Brett Hobbs <brett.hamilton.hobbs@gmail.com>
2024-07-29 19:59:25 -0400

    [REMOVE] - Template material gone

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+//import {  logo  } from "../assets";
+import { brainwave } from "../assets";
+
+/*const Header = () => {
+  return (
+
+  )
+}*/
+
+//export default Header

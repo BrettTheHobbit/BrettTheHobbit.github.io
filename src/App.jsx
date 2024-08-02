9d07dc93f0e3828f5435aaf0f15e10eea6b52e51
Brett Hobbs <bretthhobbs@gmail.com>
2024-08-01 22:22:21 -0400

    [TST] React applied to site

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Button from "./components/Button";
 
-/*const App = () => {
+const App = () => {
 
   return (
+    <h1>Testing Function</h1>
   )
+  
 }
 
-export default App*/
+export default App
